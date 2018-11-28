af82b6c1b05d0c807dbe63f587dda3f32b8165fe
remote-star <148054685@qq.com>
2018-11-28 23:49:45 +0800

    debug

--- server/index.ts
+++ server/index.ts
@@ -3,28 +3,11 @@ import * as KoaCompress from 'koa-compress'
 import * as KoaStatic from 'koa-static'
 import * as zlib from 'zlib'
 import * as historyApiFallback from 'koa2-connect-history-api-fallback'
-import * as Router from 'koa-router'
-import * as BodyParser from 'koa-bodyparser'
-import * as moment from 'moment'
-
-import exportExcel from './exporter'
 
 const app = new Koa()
-const router = new Router()
-
-router.post('/api/export', async (ctx, next) => {
-  const fileName = `${moment().format('YYYY年MM月DD日寄件单')}.xls`
-  exportExcel(ctx.request.body.orders, '../dist/' + fileName)
-  ctx.body = fileName
-})
 
 app
-  // .use(BodyParser({
-  //   formLimit: '10mb',
-  //   jsonLimit: '10mb'
-  // }))
-  // .use(router.routes())
-  // .use(router.allowedMethods())
+  .use(historyApiFallback({ whiteList: ['/api'] }))
   .use(KoaCompress({
     filter: (contentType: string) => {
       return /(text)|(javascript)/i.test(contentType)
@@ -33,5 +16,4 @@ app
     flush: zlib.Z_SYNC_FLUSH
   }))
   .use(KoaStatic('../dist'))
-  .use(historyApiFallback({ whiteList: ['/api'] }))
 app.listen(1106)
